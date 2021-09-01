47160624d2246246c34e18218d925488af7f24b9
79197 <79197@LAPTOP-FSJATN0P>
2021-09-01 22:36:24 +0300

    Добавьте файлы проекта.

---
+++ PRV_Sorts/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRV_Sorts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
