26976a7367d5cc20b258f366cc1286eaa5f3c216
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-09-02 09:12:42 +0300

    init bubble sort class

---
+++ PRV_Sorts/Bubble.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRV_Sorts
+{
+    class Bubble
+    {
+        private static Bubble instance;
+        private Bubble() { }
+
+        public Bubble GetInstance()
+        {
+            if(instance == null)
+            {
+                instance = new Bubble();
+            }
+
+            return instance;
+        }
+    }
+}
