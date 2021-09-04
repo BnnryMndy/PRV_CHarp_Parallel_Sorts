86e2054e83a772c3c2ff04034e532cefab475061
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-09-04 20:41:26 +0300

    Init Gnome sort and quick sort

---
+++ PRV_Sorts/Gnome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRV_Sorts
+{
+    class Gnome
+    {
+        private static Gnome instance;
+        private Gnome() { }
+
+        public static Gnome getInstance()
+        {
+            if (instance == null) instance = new Gnome();
+            return instance;
+        }
+
+    }
+}
