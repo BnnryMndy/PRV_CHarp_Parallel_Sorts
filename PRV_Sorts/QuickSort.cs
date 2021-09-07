6d5dd106926f9b5d2627f5ff2be8bb4253717384
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-09-07 11:31:15 +0300

    added Gnome and QuickSort

--- PRV_Sorts/QuickSort.cs
+++ PRV_Sorts/QuickSort.cs
@@ -21,7 +21,57 @@ namespace PRV_Sorts
 
         public void Sort(int[] inputArray)
         {
+            quickSort(inputArray, 0, inputArray.Length-1);
+        }
 
+        private void quickSort(int[] inputArray, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivot = partition(inputArray, low, high);
+
+                if (pivot > 1)
+                {
+                    quickSort(inputArray, low, pivot - 1);
+                }
+                if (pivot + 1 < high)
+                {
+                    quickSort(inputArray, pivot + 1, high);
+                }
+            }
+        }
+
+        private int partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right]) return right;
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+
+
+                }
+                else
+                {
+                    return right;
+                }
+            }
         }
     }
 }
